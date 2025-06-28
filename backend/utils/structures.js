5225a2ffafe02c0cbe0f4bb92b4ef90b65628cdf
Madhu Pandey <madhupandey29052004@gmail.com>
2025-06-28 11:48:07 +0530

    Fix: production errors and code cleanup for deployment

---
+++ backend/utils/structures.js
@@ -0,0 +1,28 @@
+const structures = [
+  {
+    name: "Plain",
+  },
+  {
+    name: "Striped",
+  },
+  {
+    name: "Checked",
+  },
+  {
+    name: "Floral",
+  },
+  {
+    name: "Geometric",
+  },
+  {
+    name: "Abstract",
+  },
+  {
+    name: "Solid",
+  },
+  {
+    name: "Patterned",
+  },
+];
+
+module.exports = structures; 
